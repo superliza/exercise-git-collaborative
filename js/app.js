6b77e81bf303ed989ecc33e7f543d09e59c9830e
superliza <eli_darko@hotmail.com>
2018-02-13 17:49:06 -0600

    1011

--- js/app.js
+++ js/app.js
@@ -10,4 +10,20 @@ function readFile() {
     realTimeImage.src = e.target.result;
     // console.log(e.target);
   }
-  
+
+  // Obteniendo los elemtos
+var realTimeComment = document.getElementById("real-time-comment");
+var area = document.getElementById("text-comment");
+var sendComment = document.getElementById("send");
+var sizeText = document.getElementsByClassName("size-text");
+var alignText = document.getElementsByClassName("align-text");
+var textColor = document.getElementById("text-color");
+var backgroundColor = document.getElementById("background-color");
+var commentedPost = document.getElementById("comment-posted");
+var selectImage = document.getElementById("select-image");
+// Asignando eventos a los elementos
+area.addEventListener("keyup", getTextPreview);
+sendComment.addEventListener("click", printComments);
+textColor.addEventListener("click", changeTextColor);
+backgroundColor.addEventListener("click", chnageBackgroundColor);
+selectImage.addEventListener("change", readFile);
